e174d58ec38b2bb9c31360a55e5b880ee0ad82ac
Ian Badan <ianbadan@live.com>
2022-12-06 17:26:08 -0300

    Commit contendo projeto

---
+++ uimodule/webapp/controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller"],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} BaseController
+     */
+    function (BaseController) {
+        "use strict";
+
+        return BaseController.extend("com.ianbadan.BGRecrutamento.controller.App", {
+            onInit() {},
+        });
+    }
+);
